337a9bced82857115e76170986665cd30cee3ba6
bilaldadi <48449447+bilaldadi@users.noreply.github.com>
2022-09-01 02:27:08 +0300

    admin edit profile page

---
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.admin_master')
+@section('admin')
+
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+            <div class="card-body">
+
+                <h4 class="card-title">Edit Profile</h4>
+            <form>
+                <div class="row mb-3">
+                    <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="name" value="{{ $editData->name }}" id="example-text-input">
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row mb-3">
+                    <label for="example-text-input" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="email" value="{{ $editData->email }}" id="example-text-input">
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row mb-3">
+                    <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="file" name="profile_image"  id="example-text-input">
+                    </div>
+                </div>
+                <!-- end row -->
+                <center>
+                <div class="row mb-3">
+                    <div class="col-sm-12">
+
+                            <label for="example-text-input" class="col-sm-2 col-form-label">New Profile Image</label><br>
+                            <img class="rounded-circle avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+
+                    </div>
+                </div>
+                </center>
+                <!-- end row -->
+                <center>
+                <input type="submit" value="submit" class="btn btn-info waves-effect waves-light">
+                </center>
+
+            </form>
+
+            </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+@endsection
